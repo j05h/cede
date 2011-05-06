4352ffba608c957bd2792cb91b395d4025f468cf
retr0h <john@dewey.ws>
2011-05-06 22:33:14 +0000

    + Few fixes to automate install

--- bin/env.bash
+++ bin/env.bash
@@ -1,4 +1,4 @@
 __FILE__=$(cd $(dirname $0) && pwd)
 PID_HOME=${__FILE__}/../var/run
 
-#[ ! -d ${PID_HOME} ] && mkdir -p ${PID_HOME} && exit 0
+# [ ! -d ${PID_HOME} ] && exec mkdir -p ${PID_HOME} && exit 0

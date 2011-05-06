10b84523941e3926bef51cb9fc6f45bed707eedb
retr0h <john@dewey.ws>
2011-05-06 14:18:25 -0700

    - corrected forwarding

--- recipes/firewall.bash
+++ recipes/firewall.bash
@@ -6,12 +6,19 @@ EXTERNAL=eth1
 ### Setup NAT;
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A POSTROUTING -o ${INTERNAL} -j MASQUERADE
-iptables -A FORWARD -i ${INTERNAL} -o ${INTERNAL} -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i ${EXTERNAL} -o eth0 -j ACCEPT
+sed -i 's/#\(net.ipv4.ip_forward=1\)/\1/' /etc/sysctl.conf
+sysctl -p
+
+iptables -t nat -A POSTROUTING -o ${EXTERNAL} -j MASQUERADE
+iptables -A FORWARD -i ${EXTERNAL} -o ${INTERNAL} -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i ${INTERNAL} -o ${EXTERNAL} -j ACCEPT
 
 ### Block all on ${EXTERNAL} but SSH;
 
-...
+#iptables -A INPUT -i lo -p all -j ACCEPT
+#iptables -A INPUT -i ${INTERNAL} -m state --state ESTABLISHED,RELATED -j ACCEPT
+#iptables -A INPUT -p tcp --tcp-option ! 2 -j REJECT --reject-with tcp-reset
+#iptables -A INPUT -p tcp -i ${EXTERNAL} --dport 22 -j ACCEPT
+#iptables -P INPUT DROP
 
-### Hosts file;
+### Enable iptables-restore;

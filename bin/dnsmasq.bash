47db342947449adcebde1942484c7e1f73cae951
retr0h <john@dewey.ws>
2011-05-06 22:04:41 -0700

    + Add itself as a nameserver to resolv.conf
    Will only add itself while running.

--- bin/dnsmasq.bash
+++ bin/dnsmasq.bash
@@ -6,17 +6,20 @@ source $(cd $(dirname $0) && pwd)/env.bash
 
 PID=${PID_HOME}/dnsmasq.pid
 CONF=${__FILE__}/../srv/dnsmasq/etc/dnsmasq.conf
+HOSTS=/etc/hosts
 
 case ${1} in
 	"start")
 		echo "INFO: Starting dnsmasq."
 
+    cat - ${HOSTS} <<<"nameserver 127.0.0.1" > ${HOSTS}
 		dnsmasq --pid-file=${PID} --conf-file=${CONF}
 	;;
 
 	"stop")
 		echo "INFO: Stopping dnsmasq."
 
+		sed -i "/nameserver 127.0.0.1/d" ${HOSTS}
 		cat ${PID} |xargs kill
 	;;
 

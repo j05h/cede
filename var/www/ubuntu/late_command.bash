e4c3cfd65b8ccde254e082f9966c7bf192c950c5
retr0h <john@dewey.ws>
2011-05-09 15:26:02 -0700

    + cleanup of ListenAddress

--- var/www/ubuntu/late_command.bash
+++ var/www/ubuntu/late_command.bash
@@ -1,10 +1,11 @@
 set -ex
 
-INTERNAL=$(ifconfig eth0 |sed -rn 's/.*r:([^ ]+) .*/\1/p')
+INTERNAL_INTERFACE=eth0
+IP=$(ifconfig ${INTERNAL_INTERFACE} |sed -rn 's/.*r:([^ ]+) .*/\1/p')
 
 exec >/root/postinstall.log
 exec 2>&1
 
 mkdir -p /root/.ssh && chmod 0700 /root/.ssh
 wget -O /root/.ssh/authorized_keys http://build-host/authorized_keys && chmod 0600 /root/.ssh/authorized_keys
-sed -i "s/ListenAddress 0.0.0.0/&\nListenAddress ${INTERNAL}/" /etc/ssh/sshd_config
+sed -i "s/ListenAddress 0.0.0.0/&\nListenAddress ${IP}/" /etc/ssh/sshd_config

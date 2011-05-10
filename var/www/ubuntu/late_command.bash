45870b8e6955df1d0ffd8f01832ad7b2d096f8f7
retr0h <john@dewey.ws>
2011-05-09 17:46:17 -0700

    + Take 3 trying to get ssh onto admin interface

--- var/www/ubuntu/late_command.bash
+++ var/www/ubuntu/late_command.bash
@@ -1,11 +1,10 @@
 set -ex
 
-INTERNAL_INTERFACE=eth0
-IP=$(ifconfig ${INTERNAL_INTERFACE} |sed -rn 's/.*r:([^ ]+) .*/\1/p')
-
 exec >/root/postinstall.log
 exec 2>&1
 
-mkdir -p /root/.ssh && chmod 0700 /root/.ssh
+IP=$(host $(hostname -f) |awk '{print $NF}')
+
+mkdir /root/.ssh && chmod 0700 /root/.ssh
 wget -O /root/.ssh/authorized_keys http://build-host/authorized_keys && chmod 0600 /root/.ssh/authorized_keys
 sed -i "s/ListenAddress 0.0.0.0/&\nListenAddress ${IP}/" /etc/ssh/sshd_config

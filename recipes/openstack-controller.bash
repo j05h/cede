6f6ef9371901f6cea9cd107f3d68877f4080f58d
Kevin Bringard <kevin.bringard@attinteractive.com>
2011-05-10 08:47:08 -0600

    Added some mysql stuff

--- recipes/openstack-controller.bash
+++ recipes/openstack-controller.bash
@@ -18,10 +18,13 @@ public_interface="eth0"
 vlan_interface="eth1"
 nova_conf_tmpl="nova.conf.tmpl"
 
-. controller_env
+if [ -f controller_env ]; then
+  . controller_env
+fi
 
 if [ -z $nova_user ] || [ -z $nova_pass ]; then
   echo "You must specify your nova username and password"
+  echo "It's recommended that you do this in controller_env"
   exit 1
 fi
 
@@ -31,6 +34,7 @@ apt-get -y install nova-api nova-objectstore nova-scheduler euca2ools unzip mysq
 
 service libvirt-bin restart
 
+# Fill out our nova.conf with the appropriate values
 cat $nova_conf_tmpl > nova.conf
 sed -i "s/%USER%/$nova_user/g" nova.conf
 sed -i "s/%PASS%/$nova_pass/g" nova.conf
@@ -41,6 +45,13 @@ sed -i "s/%VLANINTERFACE%/$vlan_interface/g" nova.conf
 sed -i "s/%PUBLICINTERFACE%/$public_interface/g" nova.conf
 sed -i "s/%GLANCE%/$glance_host/g" nova.conf
 sed -i "s/%SUBNET%/$my_subnet/g" nova.conf
+
+# Copy it into place
+cp -f nova.conf /etc/nova/nova.conf
+
+# We need to tell mysql to listen on all interfaces (not just localhost)
+sed -i "s/0.0.0.0/127.0.0.1/" /etc/mysql/my.cnf
+service mysql restart
 
 if [ ! -f "/etc/dnsmasq.conf" ]; then
   touch /etc/dnsmasq.conf

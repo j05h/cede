abf8e303ab67eb95047dc31c97777d2eb23547e6
Kevin Bringard <kevin.bringard@attinteractive.com>
2011-05-10 10:14:12 -0600

    Updated to create dirs in home dir

--- recipes/get-image.bash
+++ recipes/get-image.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 r="maverick"
-mkdir -p images/$r
-cd images/$r
+mkdir -p ~/images/$r
+cd ~/images/$r
 wget http://uec-images.ubuntu.com/$r/current/$r-server-uec-amd64.tar.gz
 
 tar -zxvf $r-server-uec-amd64.tar.gz

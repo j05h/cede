e852e9ed470d150a64d8d1b4f981130c8276d535
Kevin Bringard <kevin.bringard@attinteractive.com>
2011-06-23 11:51:55 -0600

    Added script to copy nova.conf to nodes

---
+++ recipes/copy-nova-conf.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+key=""
+
+if [ ! -f $key ]; then
+  echo "I couldn't find your key file, have you set it correctly?"
+  exit
+fi
+
+which nova-manage >> /dev/null
+if [ $? == 1 ]; then
+  echo "I couldn't find your nova-manage command, this should be run on the controller node"
+  exit
+fi
+
+# First we restart our local services to make sure the new nova.conf is in effect
+echo "Restarting local nova services to make sure the changesd have taken effect"
+for i in $( ls /etc/init.d/nova-* | awk -F/ '{ print $NF }' ); do echo $i; service $i restart; done
+
+# Then we copy it to all the compute instances and restart compute
+for host in $( nova-manage service list | grep compute | awk '{ print $1 }' ); do
+  address="${host}"
+  echo "Copying nova.conf to compute node $host and restarting compute..."
+  scp -o "StrictHostKeyChecking no" -i $key /etc/nova/nova.conf root@${address}:/etc/nova/nova.conf
+  ssh -o "StrictHostKeyChecking no" -i $key root@$address "service nova-compute restart"
+done

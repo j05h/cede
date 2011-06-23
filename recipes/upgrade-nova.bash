678f5dee194a620ff78023046598c6944d9f7ce9
Kevin Bringard <kevin.bringard@attinteractive.com>
2011-06-23 14:00:31 -0600

    Script to mass update nova nodes

---
+++ recipes/upgrade-nova.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# This makes a couple of assumptions, but the main one is that your "controller" node
+# runs nova-network, nova-api, nova-* that's not nova-compute
+# You'll need to modify it if you have a different layout
+
+command="apt-get update && apt-get -y dist-upgrade"
+key="/root/.ssh/key"
+
+if [ ! -f $key ]; then
+  echo "I couldn't find your key file, have you set it correctly?"
+  exit
+fi
+
+which nova-manage >> /dev/null
+if [ $? == 1 ]; then
+  echo "I couldn't find your nova-manage command, this should be run on the controller node"
+  exit
+fi
+
+# Upgrading nova locally first
+echo $command | sh
+echo "If nova was upgraded you'll probably want to run nova-manage db sync"
+
+# Then we copy it to all the compute instances and restart compute
+for host in $( nova-manage service list | grep compute | awk '{ print $1 }' ); do
+  address="${host}"
+  ssh -o "StrictHostKeyChecking no" -i $key ${address} "$command"
+done

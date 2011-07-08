6d2783f5ab44c9843820b75613c0e647b10ef5ed
retr0h <john@dewey.ws>
2011-07-08 15:35:08 -0700

    Added vlan interface
    Can override with:
    
    VLAN_INTERFACE=eth2 ./openstack-compute.bash

--- recipes/openstack-compute.bash
+++ recipes/openstack-compute.bash
@@ -2,6 +2,8 @@
 
 set -e
 
+VLAN_INTERFACE=${VLAN_INTERFACE-"eth1"}
+
 if [ ! -f /etc/apt/sources.list.d/nova-core-trunk-maverick.list ]; then
   if [ ! -x ./openstack-repo.bash ]; then
     chmod +x ./openstack-repo.bash
@@ -22,4 +24,16 @@ apt-get -y remove apache2-mpm-worker apache2-utils apache2.2-bin apache2.2-commo
 
 apt-get -y upgrade
 
-echo "You will need to copy your nova.conf in place, add your vlan interface (eth1 maybe?) and restart compute"
+if [ $(grep -c ${VLAN_INTERFACE} /etc/network/interfaces) -eq 0 ]; then
+cat<<EOF>>/etc/network/interfaces
+
+auto ${VLAN_INTERFACE}
+iface ${VLAN_INTERFACE} inet manual
+up ifconfig ${VLAN_INTERFACE} up
+EOF
+fi
+
+# service networking restart -- doesn't work, meh :/
+/etc/init.d/networking restart
+
+echo "You will need to copy your nova.conf in place, and restart compute"

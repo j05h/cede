1dd34bcb806ac63feef0fac6d5b92cf74c25171e
retr0h <john@dewey.ws>
2011-07-14 10:36:17 -0700

    Ability to pass in args to tcollector

--- recipes/tcollector.bash
+++ recipes/tcollector.bash
@@ -2,26 +2,21 @@
 
 set -e
 
-# Make sure we have the right info
-if [ -z $1 ]; then
-  echo "You must provide the collector host to use"
-  echo "$0 collect.example.com"
-  exit
+if [ -z ${1} ]; then
+	echo "Usage: ${0} <collector host> [args]"
+	exit 1
 fi
 
-# Read in the command line args
-TSD_HOST=$1
-
-# Set some paths
+TSD_HOST=${1}
 TCOL_BASE="/opt"
 GIT_URL="https://github.com/stumbleupon/tcollector.git"
-CHECKOUT_DIR=$( echo $GIT_URL | awk -F/ '{ print $NF }' | awk -F. '{ print $1 }' )
-TCOLLECTOR_PATH=$TCOL_BASE/$CHECKOUT_DIR
+CHECKOUT_DIR=$( echo ${GIT_URL} |awk -F/ '{ print $NF }' |awk -F. '{ print $1 }' )
+TCOLLECTOR_PATH=${TCOL_BASE}/${CHECKOUT_DIR}
+shift
 
-# Check out the code and cd to the tcolletor directory
-cd $TCOL_BASE
-git clone $GIT_URL
-cd $CHECKOUT_DIR
+cd ${TCOL_BASE}
+git clone ${GIT_URL}
+cd ${CHECKOUT_DIR}
 
 echo "description \"Tcollector agent\"
 author \"Kevin Bringard <kbringard@att.com>\"
@@ -31,6 +26,9 @@ stop on runlevel [016]
 
 respawn
 
-exec su -c \"env TSD_HOST=$TSD_HOST TCOLLECTOR_PATH=$TCOLLECTOR_PATH $TCOLLECTOR_PATH/startstop start\" root" >> /etc/init/tcollector.conf
+exec su -c \"env TSD_HOST=${TSD_HOST} TCOLLECTOR_PATH=${TCOLLECTOR_PATH} ${TCOLLECTOR_PATH}/startstop start $@\" root" >> /etc/init/tcollector.conf
 
 start tcollector
+
+
+exit 0

78ffc5fb089ab2c5a29f21e1e2859a63a224316b
Kevin Bringard <kevin.bringard@attinteractive.com>
2011-07-15 11:54:29 -0600

    Changed to just create networks and not projects

--- recipes/create-network.bash
+++ recipes/create-network.bash
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 
-echo "You probably want to use create-network.bash, and not this script."
-echo "You can remove these echo lines and the exit if you really mean to use it."
-exit 1
-
 set -e
 
-args=$( getopt :c:n:i:p:v:d: $* )
+args=$( getopt :c:n:i:v:d: $* )
 
 mysql_user="nova"
+
+if [ -f .creds ]; then
+  source .creds
+else
+  echo "You need to put your mysql_pass in .creds"
+  echo "The following should work: echo \"mysql_pass='p@ss'\" > .creds"
+  exit
+fi
 
 usage() {
   echo "USAGE: $0 [options]
   -c  CIDR block to allocate network(s) into
   -n  Number of networks to create
   -i  IPs to allocate per network
-  -p  Project to create
   -v  VLAN to assign (defaults to id + 9)
   -d  VLAN device (which interface do we vlan tag on)
 
   Example:
-  $0 -c 10.4.1.0/24 -n 1 -i 256 -p project -v 172 -d eth1
+  $0 -c 10.4.1.0/24 -n 1 -i 256 -v 172 -d eth1
   "
   exit 1
 }
@@ -34,7 +37,6 @@ for i; do
     -c) shift; cidr=$1; shift;;
     -n) shift; networks=$1; shift;;
     -i) shift; ips=$1; shift;;
-    -p) shift; project=$1; shift;;
     -v) shift; vlan=$1; shift;;
     -d) shift; device=$1; shift;;
   esac
@@ -53,22 +55,16 @@ bridge="br_${a}_${b}"
 
 # vlan is id + 9 unless it was specified with -v
 if [ -z $vlan ]; then
-  vlan="id + 9"
+  id=$( echo "select id from networks order by id desc limit 1" > $$.sql && mysql -u nova --password=${mysql_pass} nova < $$.sql | tail -1  && rm -f $$.sql ) && id=$(( $id + 1 ))
+  vlan=$(( id + 9 ))
 fi
 
 name="vlan${vlan}"
+
+# echo "$nova_manage network create $name $cidr $networks $ips $vlan 0 0 0 0 $device"
 
 echo "$nova_manage network create $name $cidr $networks $ips $vlan 0 0 0 0 $device" | sh
 
 echo "set @id = ( select id from networks order by id desc limit 1 ); update networks set bridge = '$bridge' where id = @id; update fixed_ips set reserved = 1 where network_id = @id and address regexp '(\\.255$)';" > /tmp/$$.sql
 
-mysql -u $mysql_user -p $mysq_pass nova < /tmp/$$.sql
-
-echo "$nova_manage project create $project root" | sh
-
-mkdir -p ~/creds/$project/root
-cd ~/creds/$project/root
-$nova_manage project zipfile $project root
-unzip nova.zip
-. novarc
-euca-run-instances $( euca-describe-images | grep machine | tail -1 | awk '{ print $2 }' )
+mysql -u $mysql_user --password="$mysql_pass" nova < /tmp/$$.sql
